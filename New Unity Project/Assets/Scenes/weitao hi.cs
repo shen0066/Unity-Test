9b0cbe95693dd96cc7db8a65301be2925e33b442
Weitao Shen <shen0066@algonquinlive.com>
2021-10-12 15:39:57 -0400

    new 111

--- New Unity Project/Assets/Scenes/weitao hi.cs
+++ New Unity Project/Assets/Scenes/weitao hi.cs
@@ -7,7 +7,7 @@ public class weitaohi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        111
     }
 
     // Update is called once per frame
